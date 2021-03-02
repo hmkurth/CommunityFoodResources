34a95c685aec62c422fed8da4cbc26bfde75d0bc
heather kurth <hmkurth@madisoncollege.edu>
2021-03-02 17:51:47 -0600

    cleaning up log, stacktraces

--- src/test/java/com/hmkurth/test/util/Database.java
+++ src/test/java/com/hmkurth/test/util/Database.java
@@ -43,11 +43,11 @@ public class Database {
         try {
             properties.load (this.getClass().getResourceAsStream("/database.properties"));
         } catch (IOException ioe) {
-            System.out.println("Database.loadProperties()...Cannot load the properties file");
-            ioe.printStackTrace();
+            logger.error("Database.loadProperties()...Cannot load the properties file", ioe);
+
         } catch (Exception e) {
-            System.out.println("Database.loadProperties()..." + e);
-            e.printStackTrace();
+            logger.error("Database.loadProperties()..." + e);
+
         }
 
     }
@@ -80,7 +80,7 @@ public class Database {
             try {
                 connection.close();
             } catch (SQLException e) {
-                System.out.println("Cannot close connection" + e);
+                logger.error("Cannot close connection" + e);
             }
         }
 
