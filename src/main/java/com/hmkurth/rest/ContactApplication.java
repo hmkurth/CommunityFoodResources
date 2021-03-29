67652b5e1f6c2636934583e2b9d06b5cb20aa822
heather kurth <hmkurth@madisoncollege.edu>
2021-03-28 19:07:42 -0500

    added rest service for contacts

---
+++ src/main/java/com/hmkurth/rest/ContactApplication.java
@@ -0,0 +1,21 @@
+package com.hmkurth.rest;
+
+
+//Defines the base URI for all resource URIs.
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/services") //You may want to add a value here so that all traffic isn't routed to the class below.
+//The java class declares root resource and provider classes
+public class ContactApplication extends Application {
+    //The method returns a non-empty collection with classes, that must be included in the published JAX-RS application
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet h = new HashSet<Class<?>>();
+        h.add(ContactService.class );
+        return h;
+    }
+}
---
+++ src/main/java/com/hmkurth/rest/ContactService.java
@@ -0,0 +1,33 @@
+package com.hmkurth.rest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hmkurth.entity.Contact;
+import com.hmkurth.persistence.GenericDao;
+import lombok.extern.log4j.Log4j2;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import java.util.List;
+@Log4j2
+@Path("/contact")
+public class ContactService {
+    // The Java method will process HTTP GET requests
+    @GET
+    // The Java method will produce content identified by the MIME Media type "application/json"
+    @Produces("application/json")
+    public Response getContact() throws JsonProcessingException {
+        // Return contact details from dao
+        GenericDao dao = new GenericDao(Contact.class);
+        List<Contact> contacts = dao.getAll();
+        //Creating the ObjectMapper object
+        ObjectMapper mapper = new ObjectMapper();
+        //Converting the Object to JSONString
+        String jsonString = mapper.writeValueAsString(contacts);
+        log.info(jsonString);
+
+        return Response.status(200).entity(jsonString).build();
+    }
+}
