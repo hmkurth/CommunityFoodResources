f772355e2e1a6132cad3708cd9e0d588f3fdee3e
heather kurth <hmkurth@madisoncollege.edu>
2021-07-07 19:37:52 -0500

    having login problems for non admin users, not sure what ptompted that change...

--- src/main/java/com/hmkurth/controller/LoginAction.java
+++ src/main/java/com/hmkurth/controller/LoginAction.java
@@ -37,23 +37,27 @@ public class LoginAction extends HttpServlet {
  **/
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
    String url;
-    logger.info("The logged in user; " + req.getRemoteUser() + "has a role of  'user' : "  + req.isUserInRole("user") );
-    //if the user is an admin, a special section is shown on the index,, otherwise go to index TODO delete extra code depending on how you want to direct this
-     if(req.isUserInRole("admin")) {
-          url = "/index.jsp";//changing to index with EL lang for admin control link
-         GenericDao dao= new GenericDao(User.class);
-         List all = dao.getAll();
-         req.setAttribute("AllUsers", all);
+
+
+    //set the user in the session attribute so it's available for other pages to access
+    if(req.getRemoteUser() != null) {
+        GenericDao dao = new GenericDao(User.class);
+        List all = dao.getAll();
+        req.setAttribute("AllUsers", all);
+        req.getSession().setAttribute("loggedInUser", req.getRemoteUser());
+        //if the user is an admin, a special section is shown on the index,, otherwise go to index TODO delete extra code depending on how you want to direct this
+    }
+         if(req.isUserInRole("admin")) {
+             url = "/admin/adminHome.jsp";//changing to index with EL lang for admin control link
+             logger.info("The logged in user; " + req.getRemoteUser() + "has a role of  'user' : "  + req.isUserInRole("user") );
+
         } else {
           url = "/index.jsp";
-     }
-     //set the user in the session attribute so it's available for other pages to access
-    if(req.getRemoteUser() != null) {
-        req.getSession().setAttribute("loggedInUser", req.getRemoteUser());
-    }
+        }
 
 
-    RequestDispatcher dispatcher = req.getRequestDispatcher("/loginThanks.jsp");
+
+    RequestDispatcher dispatcher = req.getRequestDispatcher(url);
     dispatcher.forward(req, resp);
 
     }
